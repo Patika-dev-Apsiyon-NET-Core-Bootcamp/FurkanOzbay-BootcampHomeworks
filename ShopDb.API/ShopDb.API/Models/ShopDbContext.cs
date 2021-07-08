2ceea4e9b34babf33ea4fa41803718541aaab995
furkanozb <furkanozb37@gmail.com>
2021-07-08 17:14:53 +0300

    API Projesi Eklendi

---
+++ ShopDb.API/ShopDb.API/Models/ShopDbContext.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace ShopDb.API.Models
+{
+    public partial class ShopDbContext : DbContext
+    {
+        public ShopDbContext()
+        {
+        }
+
+        public ShopDbContext(DbContextOptions<ShopDbContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Category> Category { get; set; }
+        public virtual DbSet<Product> Product { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>(entity =>
+            {
+                entity.Property(e => e.Name).HasMaxLength(60);
+            });
+
+            modelBuilder.Entity<Product>(entity =>
+            {
+                entity.Property(e => e.ImageUrl).HasMaxLength(80);
+
+                entity.Property(e => e.Name).HasMaxLength(60);
+
+                entity.Property(e => e.Price).HasColumnType("money");
+
+                entity.HasOne(d => d.Category)
+                    .WithMany(p => p.Product)
+                    .HasForeignKey(d => d.CategoryId)
+                    .HasConstraintName("FK_Pruduct_CategoryId");
+            });
+        }
+    }
+}
